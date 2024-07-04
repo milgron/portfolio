a0c7cc53dd445df6dd8ff17bbcb3244878d003bc
Tomás Milgron <tomas@withanansi.com>
2024-07-04 14:49:45 +0200

    new video for frontend, and some adjustments to videos ui

--- components/PortfolioCell.vue
+++ components/PortfolioCell.vue
@@ -11,15 +11,17 @@ defineProps({
   mediaType: {
     type: [String, Boolean],
     default: null
-  },
+  }
 })
 </script>
 
 <template>
-  <section class="grid grid-cols-1 lg:grid-cols-2 gap-8 pt-8" :id="section">
+  <section
+    class="grid gap-8 pt-8 grid-cols-1 lg:grid-cols-2" :id="section"
+  >
     <div
       :class="flip ? 'lg:order-1' : 'lg:order-2'"
-      class="grid gap-4"
+      class="grid gap-4 px-8"
     >
       <div class="flex flex-col gap-4">
         <h2 class="ff-imbue text-6xl">{{ section }}</h2>
@@ -32,17 +34,28 @@ defineProps({
     <div
       v-if="mediaType"
       :class="flip ? 'lg:order-2' : 'lg:order-1'"
-      class="bg-gray-50 flex items-center justify-center"
+      class="bg-gray-50 flex overflow-hidden h-[450px] md:h-[600px] lg:h-[450px]"
     >
-      <video v-if="mediaType == 'video'" src="/uiux.mp4" preload autoplay muted loop class="" />
+      <video 
+        v-if="mediaType == 'video'" 
+        :src="`/${section}.mp4`" 
+        preload 
+        autoplay 
+        muted 
+        loop 
+        class="border border-red-500"
+        :class="`video-${section}`" 
+      />
     </div>
   </section>
 </template>
 
-<style>
-video {
-  width: 580px;
-    height: 340px;
-    object-fit: cover;
+<style>   
+.video-ui-ux {
+  transform: scale(1.54);
+}
+.video-front-end {
+  transform: scale(1.7);
+  border: 1px solid blue;
 }
 </style>
--- components/PortfolioSection.vue
+++ components/PortfolioSection.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="grid grid-cols-1 gap-12 px-8 lg:px-12">
-    <PortfolioCell section="manifesto" :flip="true"/>
+    <PortfolioCell section="manifesto" />
     <PortfolioCell section="ui-ux" media-type="video" />
-    <PortfolioCell section="front-end" :flip="true" />
+    <PortfolioCell section="front-end" media-type="video" :flip="true" />
     <PortfolioCell section="product-conception" />
   </section>
 </template>
